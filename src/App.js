6b3cc52002b3311ff504ddb923f185951214541f
n2rajo00 <128301054+n2rajo00@users.noreply.github.com>
2023-10-05 16:57:56 +0300

    noni siinä vihdoin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const URL = "https://random-data-api.com/api/v2/blood_types"
 
 function App() {
+  const [type, setType] = useState('')
+  const [rh, setRh] = useState('')
+  const [group, setGroup] = useState('')
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      console.log(response.data)
+      setType(response.data.type)
+      setRh(response.data.rh_factor)
+      setGroup(response.data.group)
+
+    }).catch((error) => {
+      alert(error)
+    })
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Random blood types</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Blood type</th>
+            <th scope="col">RH</th>
+            <th scope="col">Group</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td> {type} </td>
+            <td> {rh} </td>
+            <td> {group} </td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 }
+
 
 export default App;
